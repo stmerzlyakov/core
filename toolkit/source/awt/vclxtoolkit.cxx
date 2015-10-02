3853266e5cc9ce23a9476efa3bf372e3ad01e0ef
Douglas Mencken <dougmencken@gmail.com>
2015-10-01 21:46:38 -0400

    use C++ exceptions for non-ObjC code only
    add SAL_THROW_IfNotObjectiveC macro to include/sal/types.h and use it
    
    C++ exception stuff isn't very compatible with Objective-C
    especially for GCC NeXT 32-bit ABI
    
    for Objective-C code there's @try { } @catch ( ) { }
    
    low-level parts changed ~
        include/com/sun/star/uno/genfunc.hxx
        include/com/sun/star/uno/Reference.hxx
        include/cppuhelper/compbase.hxx
        include/cppuhelper/compbase_ex.hxx
        include/cppuhelper/implbase.hxx
        include/cppuhelper/weakagg.hxx
        include/cppuhelper/weak.hxx
        include/comphelper/weak.hxx
        include/comphelper/uno3.hxx
        codemaker/source/cppumaker/cpputype.cxx
        cppuhelper/source/factory.cxx
    
    Change-Id: Ia6f9f88bb34f417056564b77f62bd5ed1edb9f98

--- toolkit/source/awt/vclxtoolkit.cxx
+++ toolkit/source/awt/vclxtoolkit.cxx
@@ -653,11 +653,19 @@ static void SAL_CALL ToolkitWorkerFunction( void* pArgs )
     osl_setThreadName("VCLXToolkit VCL main thread");
 
     css::uno::Reference<css::lang::XMultiServiceFactory> xServiceManager;
+#ifdef __OBJC__
+    @try
+#else
     try
+#endif
     {
         xServiceManager = ::comphelper::getProcessServiceFactory();
     }
+#ifdef __OBJC__
+    @catch (...)
+#else
     catch (const css::uno::DeploymentException&)
+#endif
     {
     }
     if (!xServiceManager.is())
@@ -684,11 +692,19 @@ static void SAL_CALL ToolkitWorkerFunction( void* pArgs )
             SolarMutexGuard aGuard;
             Application::Execute();
         }
+#ifdef __OBJC__
+        @try
+#else
         try
+#endif
         {
             pTk->dispose();
         }
+#ifdef __OBJC__
+        @catch( ...)
+#else
         catch( css::uno::Exception & )
+#endif
         {
         }
         DeInitVCL();
@@ -1416,17 +1432,27 @@ css::uno::Reference< css::awt::XWindowPeer > VCLXToolkit::createSystemChild( con
             aParentData.hWnd = reinterpret_cast<HWND>(nWindowHandle);
             #endif
             SolarMutexGuard aGuard;
+#ifdef __OBJC__
+            @try
+#else
             try
+#endif
             {
                 pChildWindow.reset( VclPtr<WorkWindow>::Create( &aParentData ) );
             }
+#ifdef __OBJC__
+            @catch ( ... )
+#else
             catch ( const css::uno::RuntimeException & rEx )
+#endif
             {
                 // system child window could not be created
+#ifndef __OBJC__
                 OSL_TRACE(
                     "VCLXToolkit::createSystemChild: caught %s\n",
                     OUStringToOString(
                         rEx.Message, RTL_TEXTENCODING_UTF8).getStr());
+#endif
                 pChildWindow.clear();
             }
         }
@@ -1823,16 +1849,26 @@ void VCLXToolkit::callTopWindowListeners(
             {
                 css::uno::Reference< css::awt::XTopWindowListener >
                       xListener(i, css::uno::UNO_QUERY);
+#ifdef __OBJC__
+                @try
+#else
                 try
+#endif
                 {
                     (xListener.get()->*pFn)(aAwtEvent);
                 }
+#ifdef __OBJC__
+                @catch (...)
+#else
                 catch (const css::uno::RuntimeException & rEx)
+#endif
                 {
+#ifndef __OBJC__
                     OSL_TRACE(
                         "VCLXToolkit::callTopWindowListeners: caught %s\n",
                         OUStringToOString(
                             rEx.Message, RTL_TEXTENCODING_UTF8).getStr());
+#endif
                 }
             }
         }
@@ -1869,18 +1905,28 @@ bool VCLXToolkit::callKeyHandlers(::VclSimpleEvent const * pEvent,
         {
             css::uno::Reference< css::awt::XKeyHandler > xHandler(
                 i, css::uno::UNO_QUERY);
+#ifdef __OBJC__
+            @try
+#else
             try
+#endif
             {
                 if ((bPressed ? xHandler->keyPressed(aAwtEvent)
                       : xHandler->keyReleased(aAwtEvent)))
                     return true;
             }
+#ifdef __OBJC__
+            @catch (...)
+#else
             catch (const css::uno::RuntimeException & rEx)
+#endif
             {
+#ifndef __OBJC__
                 OSL_TRACE(
                     "VCLXToolkit::callKeyHandlers: caught %s\n",
                     OUStringToOString(
                        rEx.Message, RTL_TEXTENCODING_UTF8).getStr());
+#endif
             }
         }
     }
@@ -1919,17 +1965,27 @@ void VCLXToolkit::callFocusListeners(::VclSimpleEvent const * pEvent,
             {
                 css::uno::Reference< css::awt::XFocusListener > xListener(
                     i, css::uno::UNO_QUERY);
+#ifdef __OBJC__
+                @try
+#else
                 try
+#endif
                 {
                     bGained ? xListener->focusGained(aAwtEvent)
                         : xListener->focusLost(aAwtEvent);
                 }
+#ifdef __OBJC__
+                @catch (...)
+#else
                 catch (const css::uno::RuntimeException & rEx)
+#endif
                 {
+#ifndef __OBJC__
                     OSL_TRACE(
                         "VCLXToolkit::callFocusListeners: caught %s\n",
                         OUStringToOString(
                             rEx.Message, RTL_TEXTENCODING_UTF8).getStr());
+#endif
                 }
             }
         }
